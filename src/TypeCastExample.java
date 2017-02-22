66b4c4984f3ec18ba41fc3cf036103b3b0b316c7
Sid498 <sidpawar619@gmail.com>
2017-02-22 19:37:28 +0530

    Assignment_7.1

---
+++ src/TypeCastExample.java
@@ -0,0 +1,10 @@
+public class TypeCastExample {
+
+	public static void main(String[] args) {
+		int number = 10;
+		String Text = "";
+		Text = String.valueOf(number);//Converting int to string using String.ValueOf().
+		System.out.println(Text);
+	}
+
+}
